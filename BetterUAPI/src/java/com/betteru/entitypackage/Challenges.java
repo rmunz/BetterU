c5fff1131ce774966746736fedbbaa5391d2431e
Ojas <ojmhetar@gmail.com>
2016-04-03 19:31:25 -0400

    reset to reflect the db changes

---
+++ BetterUAPI/src/java/com/betteru/entitypackage/Challenges.java
@@ -0,0 +1,155 @@
+/*
+ * Created by Ojas Mhetar on 2016.04.02  * 
+ * Copyright © 2016 Ojas Mhetar. All rights reserved. * 
+ */
+package com.betteru.entitypackage;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ojmhetar
+ */
+@Entity
+@Table(name = "Challenges")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Challenges.findAll", query = "SELECT c FROM Challenges c"),
+    @NamedQuery(name = "Challenges.findByName", query = "SELECT c FROM Challenges c WHERE c.name = :name"),
+    @NamedQuery(name = "Challenges.findByDescription", query = "SELECT c FROM Challenges c WHERE c.description = :description"),
+    @NamedQuery(name = "Challenges.findByChallengeType", query = "SELECT c FROM Challenges c WHERE c.challengeType = :challengeType"),
+    @NamedQuery(name = "Challenges.findByPointsAwarded", query = "SELECT c FROM Challenges c WHERE c.pointsAwarded = :pointsAwarded"),
+    @NamedQuery(name = "Challenges.findByInd", query = "SELECT c FROM Challenges c WHERE c.ind = :ind"),
+    @NamedQuery(name = "Challenges.findByGoalType", query = "SELECT c FROM Challenges c WHERE c.goalType = :goalType")})
+public class Challenges implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "Name")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "Description")
+    private String description;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "ChallengeType")
+    private String challengeType;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PointsAwarded")
+    private int pointsAwarded;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "Ind")
+    private int ind;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "GoalType")
+    private int goalType;
+
+    public Challenges() {
+    }
+
+    public Challenges(String name) {
+        this.name = name;
+    }
+
+    public Challenges(String name, String description, String challengeType, int pointsAwarded, int ind, int goalType) {
+        this.name = name;
+        this.description = description;
+        this.challengeType = challengeType;
+        this.pointsAwarded = pointsAwarded;
+        this.ind = ind;
+        this.goalType = goalType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getChallengeType() {
+        return challengeType;
+    }
+
+    public void setChallengeType(String challengeType) {
+        this.challengeType = challengeType;
+    }
+
+    public int getPointsAwarded() {
+        return pointsAwarded;
+    }
+
+    public void setPointsAwarded(int pointsAwarded) {
+        this.pointsAwarded = pointsAwarded;
+    }
+
+    public int getInd() {
+        return ind;
+    }
+
+    public void setInd(int ind) {
+        this.ind = ind;
+    }
+
+    public int getGoalType() {
+        return goalType;
+    }
+
+    public void setGoalType(int goalType) {
+        this.goalType = goalType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (name != null ? name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Challenges)) {
+            return false;
+        }
+        Challenges other = (Challenges) object;
+        if ((this.name == null && other.name != null) || (this.name != null && !this.name.equals(other.name))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.betteru.entitypackage.Challenges[ name=" + name + " ]";
+    }
+    
+}
---
+++ BetterUAPI/src/java/com/betteru/entitypackage/service/ProgressFacadeREST.java
@@ -0,0 +1,90 @@
+/*
+ * Created by Ojas Mhetar on 2016.04.02  * 
+ * Copyright © 2016 Ojas Mhetar. All rights reserved. * 
+ */
+package com.betteru.entitypackage.service;
+
+import com.betteru.entitypackage.Progress;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author ojmhetar
+ */
+@Stateless
+@Path("com.betteru.entitypackage.progress")
+public class ProgressFacadeREST extends AbstractFacade<Progress> {
+
+    @PersistenceContext(unitName = "BetterUAPIPU")
+    private EntityManager em;
+
+    public ProgressFacadeREST() {
+        super(Progress.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Progress entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Integer id, Progress entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Integer id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Progress find(@PathParam("id") Integer id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
