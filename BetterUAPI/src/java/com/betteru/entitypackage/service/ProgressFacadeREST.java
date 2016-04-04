da3c910c507798a093a1c77608c3e04902f7d6eb
jmbinger <juliabinger94@gmail.com>
2016-04-04 19:02:09 -0400

    first attempt at rest endpoints for progress

--- BetterUAPI/src/java/com/betteru/entitypackage/service/ProgressFacadeREST.java
+++ BetterUAPI/src/java/com/betteru/entitypackage/service/ProgressFacadeREST.java
@@ -18,6 +18,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
+import javax.persistence.TypedQuery;
+
 
 /**
  *
@@ -82,6 +85,97 @@ public class ProgressFacadeREST extends AbstractFacade<Progress> {
         return String.valueOf(super.count());
     }
 
+    @GET
+    @Path("{id}/caloriesIn/week/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getCaloriesInWeek(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdCaloriesInByWeek", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+
+    @GET
+    @Path("{id}/caloriesIn/month/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getCaloriesInMonth(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdCaloriesInByMonth", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/caloriesOut/week/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getCaloriesOutWeek(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByCaloriesOutWeek", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/caloriesOut/month/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getCaloriesOutMonth(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByCaloriesOutMonth", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/weight/week/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getWeightWeek(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByWeightWeek", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    
+    @GET
+    @Path("{id}/weight/month/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getWeightMonth(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByWeightMonth", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/steps/week/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getStepsWeek(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByStepsWeek", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/steps/month/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getStepsMonth(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByStepsMonth", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/miles/week/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getMilesWeek(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByMilesWeek", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
+    @GET
+    @Path("{id}/miles/month/{day}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Progress> getMilesMonth(@PathParam("id") Integer id, @PathParam("day") Date day) {
+        TypedQuery<Progress> query = em.createNamedQuery("Progress.findIdByMilesMonth", Progress.class);
+        List<Progress> results = query.getResultList();
+        return results;
+    }
+    
     @Override
     protected EntityManager getEntityManager() {
         return em;
