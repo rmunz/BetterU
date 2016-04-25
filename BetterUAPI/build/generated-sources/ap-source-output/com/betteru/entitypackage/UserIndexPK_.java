6814d43b98d863e47fa0fcd9001c9aa3e4ba85ca
tstreetcs <tstreet@vt.edu>
2016-04-25 17:32:33 -0400

    Commit before pull request

---
+++ BetterUAPI/build/generated-sources/ap-source-output/com/betteru/entitypackage/UserIndexPK_.java
@@ -0,0 +1,14 @@
+package com.betteru.entitypackage;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-25T17:30:15")
+@StaticMetamodel(UserIndexPK.class)
+public class UserIndexPK_ { 
+
+    public static volatile SingularAttribute<UserIndexPK, String> challengeType;
+    public static volatile SingularAttribute<UserIndexPK, Integer> userID;
+
+}
---
+++ BetterUAPI/build/generated-sources/ap-source-output/com/betteru/entitypackage/WeeklyChallenges_.java
@@ -0,0 +1,16 @@
+package com.betteru.entitypackage;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-25T17:30:15")
+@StaticMetamodel(WeeklyChallenges.class)
+public class WeeklyChallenges_ { 
+
+    public static volatile SingularAttribute<WeeklyChallenges, String> description;
+    public static volatile SingularAttribute<WeeklyChallenges, Integer> pointsAwarded;
+    public static volatile SingularAttribute<WeeklyChallenges, String> challengeName;
+    public static volatile SingularAttribute<WeeklyChallenges, Integer> ind;
+
+}
