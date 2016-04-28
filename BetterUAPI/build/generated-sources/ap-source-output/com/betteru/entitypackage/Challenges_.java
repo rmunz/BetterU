c58994fcd445cbbcc66bce129d52769c5eeeed63
Julia Binger <juliabinger94@gmail.com>
2016-04-28 15:47:19 -0400

    Update Challenges_.java

--- BetterUAPI/build/generated-sources/ap-source-output/com/betteru/entitypackage/Challenges_.java
+++ BetterUAPI/build/generated-sources/ap-source-output/com/betteru/entitypackage/Challenges_.java
@@ -4,11 +4,8 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-05T11:41:51")
-=======
+
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-07T14:57:02")
->>>>>>> master
 @StaticMetamodel(Challenges.class)
 public class Challenges_ { 
 
