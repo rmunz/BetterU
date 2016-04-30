7fc514f353a3541bfa50602bbb54ca5e3af2a4a4
brobohn <brobohn@vt.edu>
2016-04-30 14:58:39 -0400

    Initial Scheduling.

---
+++ BetterUWeb/src/java/com/betteru/managers/ScheduleManager.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Ben Robohn on 2016.04.30  * 
+ * Copyright © 2016 Osman Balci. All rights reserved. * 
+ */
+package com.betteru.managers;
+
+/**
+ *
+ * @author Ben
+ */
+public class ScheduleManager {
+    
+}
