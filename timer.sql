4ce26b3fc8d21af74653873badb12cdc8297e285
Ojas Mhetar <ojmhetar@gmail.com>
2016-05-04 08:49:33 -0400

    reference user weight

--- timer.sql
+++ timer.sql
@@ -3,4 +3,4 @@ CREATE EVENT progressEntryCreation
       EVERY 1 DAY
       STARTS '2016-05-04 00:00:00' ON COMPLETION PRESERVE ENABLE 
     DO
-      INSERT INTO BetterU.Progress(UserId, LogDate, CaloriesIn, CaloriesOut, Weight, Miles, Steps) SELECT DISTINCT id, UNIX_TIMESTAMP(CURDATE()), '0', '0', '0', '0', '0' FROM User WHERE NOT EXISTS (Select 1 FROM Progress WHERE logDate=UNIX_TIMESTAMP(CURDATE()) AND UserId=id);
+      INSERT INTO BetterU.Progress(UserId, LogDate, CaloriesIn, CaloriesOut, Weight, Miles, Steps) SELECT DISTINCT id, UNIX_TIMESTAMP(CURDATE()), '0', '0', Weight, '0', '0' FROM User WHERE NOT EXISTS (Select 1 FROM Progress WHERE logDate=UNIX_TIMESTAMP(CURDATE()) AND UserId=id);
