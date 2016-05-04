456bb3c64df134aedd59ec4b8937f9d00b98cfcd
Ojas Mhetar <ojmhetar@gmail.com>
2016-05-04 14:49:39 -0400

    redirect to dailyOverview

--- BetterUWeb/src/java/com/betteru/managers/LoginManager.java
+++ BetterUWeb/src/java/com/betteru/managers/LoginManager.java
@@ -101,7 +101,7 @@ public class LoginManager implements Serializable {
       if (user.getUsername().equals(getUsername()) && user.getPassword().equals(getPassword())) {
         errorMessage = "";
         initializeSessionMap(user);
-        return "MyProgress?faces-redirect=true";
+        return "DailyOverview?faces-redirect=true";
       }
       errorMessage = "Invalid username or password!";
       return "";
